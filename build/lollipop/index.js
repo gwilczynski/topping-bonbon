ce998df1691d73524c4cd0155f89b4c7d51fb8d6
Grzegorz Wilczynski <grzegorz.wilczynski@infopark.de>
2021-11-22 18:26:49 +0100

    add build

---
+++ build/lollipop/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mix = function (ingredients) {
+    return "lollipop with: [".concat(ingredients.join(", "), "]");
+};
+exports.default = mix;
