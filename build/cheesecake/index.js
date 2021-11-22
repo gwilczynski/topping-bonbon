a116a2502ec85200a89e9d284aa085c6b8340dbf
Grzegorz Wilczynski <grzegorz.wilczynski@infopark.de>
2021-11-22 18:46:22 +0100

    build

---
+++ build/cheesecake/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var make = function (ingredients) {
+    return "cheesecake with: [".concat(ingredients.join(", "), "]");
+};
+exports.default = make;
